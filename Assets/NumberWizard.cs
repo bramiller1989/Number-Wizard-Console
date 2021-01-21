ba0546f39fbdf294678d3142780af2eda91689e3
bramiller1989 <bramiller1989@gmail.com>
2021-01-20 21:25:48 -0600

    add min max variables

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -7,10 +7,13 @@ public class NumberWizard : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        int max = 1000;
+        int min = 1;
+
         Debug.Log("Welcome to Number Wizard");
         Debug.Log("Think of a number...");
-        Debug.Log("The lowest number you can pick is 1");
-        Debug.Log("The highest number you can pick is 1000");
+        Debug.Log("The lowest number you can pick is: " + min);
+        Debug.Log("The highest number you can pick is: " + max);
     }
 
     // Update is called once per frame
