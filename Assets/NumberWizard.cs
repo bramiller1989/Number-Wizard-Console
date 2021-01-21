0806a60b2265975c5ba939ffab9fee56e99ebbd3
bramiller1989 <bramiller1989@gmail.com>
2021-01-20 21:15:10 -0600

    added welcome content

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -7,7 +7,10 @@ public class NumberWizard : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Welcome to Number Wizard");
+        Debug.Log("Think of a number...");
+        Debug.Log("The lowest number you can pick is 1");
+        Debug.Log("The highest number you can pick is 1000");
     }
 
     // Update is called once per frame
